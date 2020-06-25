a2776901d4c093c20166044eb9b34241e0b62905
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-06-25 09:33:10 +0700

    .

---
+++ Session3/Example1.php
@@ -0,0 +1,12 @@
+<?php
+$Gpa = 80;
+if($Gpa < 40){
+    echo "Trượt";
+}
+else if ($Gpa > 40 && $Gpa < 80){
+    echo "Khá";
+}
+else{
+    echo "Giỏi";
+}
+?>
--- Session3/snippet6.php
+++ Session3/snippet6.php
@@ -1,15 +1,16 @@
 <?php
+error_reporting(0);
 function e()
 {
-    echo "This is e() \n";
+    echo "This is e()" . "</br>";
 
 };
 function f(){
-    echo "This is f() \n";
+    echo "This is f() \n" . "</br>";
     return e;
 };
 function g(){
-    echo "This is g() \n";
+    echo "This is g() \n"."</br>";
     return f;
 };
 g();
