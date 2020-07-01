3ba931c2d3d25c435ee89b58efc4bba5f31b4bbf
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-07-01 21:56:09 +0700

    up

---
+++ Session12/snippet8.php
@@ -0,0 +1,18 @@
+<?php
+$aphabet = "u";
+switch ($aphabet){
+    case 'a':
+        echo "<br> hello a";
+        break;
+    case "A":
+        echo "<br> hello A";
+        break;
+    case "u":
+        echo "<br> hello u";
+        break;
+    default:
+        echo "Invalid value!!";
+
+
+}
+?>
