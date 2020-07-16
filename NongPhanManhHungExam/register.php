a2cb7ffb3afa82b5d26b46c9d3eb84999405d982
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-07-16 09:41:15 +0700

    .

--- NongPhanManhHungExam/register.php
+++ NongPhanManhHungExam/register.php
@@ -11,8 +11,8 @@ if(isset($_POST['fullname']) && isset($_POST['email']) && isset($_POST['username
     $result = $conn->query($query);
     if(!$result) die("Query is fail!!!");
     else{
-        setcookie('username', $username);
-        setcookie('fullname', $fullname);
+        setcookie('username', $username, time()+3600);
+        setcookie('fullname', $fullname, time()+3600);
         header("location: welcome.php");
     }
 }
@@ -23,6 +23,14 @@ if(isset($_POST['fullname']) && isset($_POST['email']) && isset($_POST['username
     <meta charset="UTF-8">
     <title>Register</title>
     <style>
+        body{
+            text-align: center;
+            font-size: larger;
+        }
+        .submit{
+            padding: 10px;
+            font-size: 20px;
+        }
 
     </style>
 </head>
@@ -30,14 +38,14 @@ if(isset($_POST['fullname']) && isset($_POST['email']) && isset($_POST['username
 <body>
 <h1>Register</h1>
 <div class="center">
-
+    <form action="register.php" method="post">
+        Fullname  <br><input type="text" name="fullname"><br>
+        Email  <br> <input type="email" name="email"><br>
+        Username  <br><input type="text" name="username"><br>
+        Password <br><input type="password" name="password"><br><br>
+        <input type="submit" value="Register" class="submit">
+    </form>
 </div>
-<form action="register.php" method="post">
-    Fullname <br><input type="text" name="fullname"><br>
-    Email <br> <input type="email" name="email"><br>
-    Username <br><input type="text" name="username"><br>
-    Password <br><input type="password" name="password"><br><br>
-    <input type="submit" value="Register">
-</form>
+
 </body>
 </html>
